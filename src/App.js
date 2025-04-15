fb7939931cc1ca569df7475236da90b7e15ed895
priyanka rajana <priyanka.rajana@drcsystems.com>
2025-04-15 16:57:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,61 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Greet from './components/Greet';
+import Welcome from './components/Welcome';
+import Hello from './components/Hello'; 
+import Message from './components/Message';
+import Counter from './components/Counter';
+import FunctionClick from './components/FunctionClick'; 
+import ClassClick from './components/ClassClick';
+import ParentComponent from './components/ParentComponent';
+import EventBind from './components/EventBind';
+import UserGreeting from './components/UserGreeting';
+import NameList from './components/NameList';
+import Stylesheet from './components/Stylesheet';
+import Inline from './components/Inline';
+import './appStyles.css';
+import styles from './appStyles.module.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <h1 className='error'>Error</h1>
+      <h1 className={styles.success}>Success</h1> 
+{/* //cant access in any other child components */}
+      {/* <Greet name="Bruce" heroName="batman">
+        <p>This is children props</p>
+      </Greet>
+      <Greet name="clark" heroName="superman">
+        <button>Action</button>
+      </Greet>
+     
+      <Welcome name="Bruce" heroName="batman" />
+      <Welcome name="clark" heroName="superman" />
+      <Hello />
+      <Message />
+      <Counter /> */}
+
+
+      {/* <FunctionClick />
+      <ClassClick /> */}
+      {/* <Message />
+      <EventBind />
+      <ParentComponent /> */}
+
+      {/* <UserGreeting/> */}
+      {/* <NameList /> */}
+      <Stylesheet primary={true} />
+      <Inline />
+
+
+
+
+
+
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello world!
         </p>
         <a
           className="App-link"
@@ -17,7 +65,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
